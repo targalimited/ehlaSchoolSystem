bfbb690d2ff2ae2dcc8ad2b6504ca6fdc7992cb5
hilton <hong304@gmail.com>
2017-06-22 16:45:08 +0800

    import teacher done

--- backend/app/Http/Controllers/UserController.php
+++ backend/app/Http/Controllers/UserController.php
@@ -2,52 +2,159 @@
 
 namespace App\Http\Controllers;
 
+use App\SchoolClass;
+use App\Subject;
+use App\TeacherClassSubject;
 use App\User;
 use Illuminate\Http\Request;
 use Excel;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
-    public function postTeacher(Request $request){
+    public function postTeacher(Request $request)
+    {
 
-        if($request->hasFile('file')){
+        if ($request->hasFile('file')) {
             $path = $request->file('file')->getRealPath();
-            Excel::load($path, function($reader) {
+            Excel::load($path, function ($reader) use ($request) {
 
                 $results = $reader->get()->toArray();
 
 
+//                try {
 
-                User::insert($results[0]);
 
-                dd($results);
+                    foreach ($results[0] as $v) {
+                        $username[] = $v['username'];
+                        $email[] = $v['email'];
+                    }
+
+                    foreach (Subject::all() as $v) {
+                        $subject[$v->id] = $v->s_name_en;
+                    }
+
+                    foreach (SchoolClass::all() as $v) {
+                        $class[$v->id] = $v->c_name;
+                    }
+
+                    $i=0;
+                $errors = [];
+
+                    foreach ($results[1] as $v) {
+
+                        if (!in_array($v['username'], $username)) {
+                            $errors[$i] = 'No this username ' . $v['username'];
+                            $i++;
+                        }
+                        if (!in_array($v['email'], $email)) {
+                            $errors[$i] = 'No this email ' . $v['email'];
+                            $i++;
+                        }
+                        if (!in_array($v['class'], $class)) {
+                            $errors[$i] = 'No this class ' . $v['class'];
+                            $i++;
+                        }
+                        if (!in_array($v['subject'], $subject)) {
+                            $errors[$i] = 'No this subject ' . $v['subject'];
+                            $i++;
+                        }
+                    }
+
+                    if ($errors) {
+                        $result = [
+                            'status' => false,
+                            'code' => '',
+                            'message' => $errors
+                        ];
+                        dd('hi');
+                        return error_json($result);
+                    } else {
+                        foreach($results[0] as $v){
+                            $user = New User();
+                            $user->username = $v['username'];
+                            $user->email = $v['email'];
+                            $user->password = $v['password'];
+                            $user->save();
+                         }
+                        $users = User::whereIn('email',$email)->get();
+                        foreach($users as $v){
+                            $user_email[$v->id] = $v->email;
+                        }
+
+                        foreach($results[1] as $k => $v){
+                            $new_teacher_set[$k]['teacher_id']=array_search($v['email'],$user_email);
+                            $new_teacher_set[$k]['class_id']=array_search($v['class'],$class);
+                            $new_teacher_set[$k]['subject_id']=array_search($v['subject'],$subject);
+                            $new_teacher_set[$k]['comment']=$v['email'].",".$v['class'].",".$v['subject'];
+                        }
+
+                        TeacherClassSubject::insert($new_teacher_set);
+
+                    }
+
+
+
+//                    $validator = Validator::make($results[1], [
+//                        '*.username' => 'required|exists:user,username',
+//                    ]);
+//
+//                    if ($validator->fails()) {
+//                        $result = [
+//                            'status' => false,
+//                            'code' => '',
+//                            'message' => $validator->errors()
+//                        ];
+//                        DB::rollBack();
+//                        return error_json($result);
+//                    }
+
+
+//                    DB::transaction(function () use ($results) {
+//
+//
+//                        $validator = Validator::make($results[1], [
+//                            '*.username' => 'required|exists:user,username',
+//                        ]);
+//
+//                        if ($validator->fails()) {
+//                            $result = [
+//                                'status' => false,
+//                                'code' => '',
+//                                'message' => $validator->errors()
+//                            ];
+//
+//                            return error_json($result);
+//                        }
+//
+//                    }, 2);
+
+//                } catch (\Exception $e) {
+//
+//                    dd('ex');
+//
+//                    $result = [
+//                        'status' => false,
+//                        'code' => $e->getCode(),
+//                        'message' => $e->getMessage()
+//                    ];
+//                    return Response()->json($result, 500);
+//                }
+
 
             });
 
 
-
-//            if(!empty($data) && $data->count()){
-//                foreach ($data as $key => $value) {
-//                    $insert[] = ['title' => $value->Sheet1];
-//                }
-//                if(!empty($insert)){
-//                    DB::table('items')->insert($insert);
-//                    dd('Insert Record successfully.');
-//                }
-//
-//            }
-
-        }else{
-            dd('hi');
+        } else {
+            dd('please select excel file to update');
         }
-
-
-
 
 
     }
 
-    public function postStudent(Request $request){
+    public function postStudent(Request $request)
+    {
 
     }
 }
--- backend/app/Http/Helpers.php
+++ backend/app/Http/Helpers.php
@@ -2,9 +2,17 @@
 
 function json($result){
     if (isset($_SERVER['HTTP_POSTMAN_TOKEN'])){
-        var_dump($result);
+        dump($result);
     }
     return Response()->json($result);
+}
+
+function error_json($result){
+    if (isset($_SERVER['HTTP_POSTMAN_TOKEN'])){
+        dump($result);
+        abort(500, 'Die in action.');
+    }
+    return Response()->json($result,500);
 }
 
 function return_success(){
