da4097a16c061b847c27fb2e6bc877f486af36c8
hilton <hong304@gmail.com>
2018-07-11 17:13:28 +0800

    commit

--- backend/app/SchoolClass.php
+++ backend/app/SchoolClass.php
@@ -17,13 +17,23 @@ class SchoolClass extends Model
         return $this->belongsToMany('App\Subject','teacher_class_subject','class_id')->withPivot('id');
     }
 
-    public function teachers()
-    {
-        return $this->belongsToMany('App\User','teacher_class_subject','class_id','teacher_id')->withPivot('id');
-    }
+//    public function teachers()
+//    {
+//        return $this->belongsToMany('App\User','teacher_class_subject','class_id','teacher_id')->withPivot('id');
+//    }
 
     public function calendarEvents(){
         return $this->belongsToMany(CalendarEvent::class);
+    }
+
+    public function students()
+    {
+        return $this->hasMany(StudentClassSubject::class,'class_id');
+    }
+
+    public function teachers()
+    {
+        return $this->hasMany(TeacherClassSubject::class,'class_id');
     }
 
 }
